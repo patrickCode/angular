7fc9796f14e4c9443385d2d6379d9309caeafff4
Pratik Bhattacharya <patrickb404@outlook.com>
2017-01-27 05:18:17 +0530

    Added simple controller Controller functionalities  - Interpolation  - Using model  - Real-time data binding  - Calling functions  - Basic form features

---
+++ Tutorial/VS/02 - Controller/TaskTracker/TaskTracker/App_Start/BundleConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Optimization;
+
+namespace TaskTracker
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/library").Include(
+                "~/Scripts/lib/jquery-{version}.js",
+                "~/Scripts/lib/jquery.validate*",
+                "~/Scripts/lib/modernizr-*",
+                "~/Scripts/lib/bootstrap.js",
+                "~/Scripts/lib/respond.js",
+                "~/Scripts/lib/respond.js",
+                "~/Scripts/lib/angular.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                "~/Scripts/app/app.js"
+                ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
---
+++ Tutorial/VS/02 - Controller/TaskTracker/TaskTracker/Scripts/app/app.js
@@ -0,0 +1,67 @@
+﻿var app = angular.module("taskTrackerApp", []);
+
+app.controller("taskController", function ($scope) {
+    $scope.applicationName = "Task Tracker";
+    $scope.companyInfo = "Microsoft";
+
+    $scope.task = {
+        title: "",
+        details: "",
+        assignedTo: "",
+        priority: "Low",
+        effort: 0,
+        estimatedDays: 0
+    }
+
+    $scope.taskSubmitted = true;
+    $scope.errorMessage = "";
+
+    $scope.users = [
+        { alias: "johnmc@microsoft.com", name: "John McClane" },
+        { alias: "nfury@microsoft.com", name: "Nick Fury" },
+        { alias: "rdeckard@microsoft.com", name: "Rick Deckard" },
+        { alias: "leon@microsoft.com", name: "Léon" },
+        { alias: "adwaller@microsoft.com", name: "Amanda Waller" }
+    ];
+
+    $scope.effortToString = function (effortValue) {
+        if (effortValue <= 5)
+            return "Low Effort";
+        if (effortValue <= 15)
+            return "Medium Effort";
+        if (effortValue <= 30)
+            return "High Effort";
+        else
+            return "Task Impossible";
+    }
+
+    $scope.getEstimatedDate = function (esitimatedDays) {
+        var currentDateMs = Date.now();
+        var estimatedMs = currentDateMs + (esitimatedDays * 24 * 60 * 60 * 1000);
+        var estimatedDate = new Date(estimatedMs).toLocaleDateString();
+        $scope.task.estimatedDate = estimatedDate;
+    }
+
+    $scope.submitTask = function () {
+        //Validations
+        $scope.taskSubmitted = false;
+        if ($scope.task.title === undefined || $scope.task.title === null || $scope.task.title === "") {
+            $scope.errorMessage = "Title cannot be blank";
+            return;
+        }
+        if ($scope.task.assignedTo === undefined || $scope.task.assignedTo === null || $scope.task.assignedTo === "") {
+            $scope.errorMessage = "Task hasn't been assigned";
+            return;
+        }
+        if ($scope.task.effort > 30) {
+            $scope.errorMessage = "Too high effort. Please consider breaking the task into multiple simpler tasks";
+            return;
+        }
+        if ($scope.task.estimatedDays > 50) {
+            $scope.errorMessage = "For a single task days cannot be greated than 50. Please consider breaking your task";
+            return;
+        }
+        $scope.taskSubmitted = true;
+        alert("Task Succesfully submitted");
+    }
+})
