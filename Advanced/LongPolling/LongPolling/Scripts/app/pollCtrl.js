6556e34d5f270707bebb2ba68bc406a6a043575c
Pratik Bhattacharya <pratikb@microsoft.com>
2017-04-25 13:22:31 +0530

    Added Long polling sample (un-tested)

---
+++ Advanced/LongPolling/LongPolling/Scripts/app/pollCtrl.js
@@ -0,0 +1,17 @@
+﻿(function (module) {
+
+    var pollController = function ($scope, pollSvc) {
+
+        $scope.startPoll = function () {
+            pollSvc.getData(1000, 1000)
+                .then(function (data) {
+                    alert(data);
+                }, function (error) {
+                    alert(error);
+                });
+        }
+    }
+
+    module.controller("pollCtrl", pollController);
+
+}(angular.module("app")))
---
+++ Advanced/LongPolling/LongPolling/Scripts/app/pollSvc.js
@@ -0,0 +1,54 @@
+﻿(function (module) {
+
+    var pollSvc = function ($http, $timeout, $q, $interval) {
+        var getData = function (maxWaitTime, maxPollTime) {
+            var deferred = $q.defer();
+            var timeEllapsed = 0;
+            var delay = 1000;
+            var keepPolling = true;
+
+            setTimeout(function () {
+                poll(deferred, delay);
+            }, 1000);
+            //$interval(, 1);
+
+            return deferred.promise;
+        }
+
+
+        var poll = function (deferred) {
+            var keepPolling = true;
+            
+            $timeout(function () {
+                $http.get("https://phoenixidentityproviderdev.azurewebsites.net/api/services/assertion/asd/status").then(function (response) {
+                    var result = processData(response.data);
+                    if (result) {
+                        keepPolling = false;
+                        deferred.resolve(data);
+                        httpCalled = true;
+                    }
+                    else {
+                        httpCalled = true;
+                        poll(deferred);
+                        deferred.notify();
+                    }
+                }, function (error) {
+                    httpCalled = true;
+                    keepPolling = false;
+                    deferred.reject(error);
+                })
+            }, 1000);
+        }
+
+        var processData = function (data) {
+            return false;
+        }
+
+        return {
+            getData: getData
+        }
+    }
+
+    module.factory("pollSvc", pollSvc);
+
+}(angular.module("app")))
